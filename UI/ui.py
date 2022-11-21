3122444955af73fd14a985dabf6fb8735e23d5d4
aleksabajat2001@gmail.com <aleksabajat2001@gmail.com>
2022-11-22 00:01:34 +0100

    Upgraded Setup -better dockerfiles -added endpoints -added js and css -added templates

--- UI/ui.py
+++ UI/ui.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
+
+@app.route('/check')
+def check():
+    return "200 - OK - UI is up!"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
