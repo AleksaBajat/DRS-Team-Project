27636aea366553cdb6e2467ef8c9bc7bbc0cec2d
AleksaBajat <aleksabajat2001@gmail.com>
2022-11-22 08:40:20 +0100

    Docker Fixes -mount bind fix -jinja refresh templates

--- UI/ui.py
+++ UI/ui.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route('/check')
 def check():
     return "200 - OK - UI is up!"
