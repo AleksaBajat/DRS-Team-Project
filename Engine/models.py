a388176c8ff3405b31351804c03816c799f46512
aleksabajat2001@gmail.com <aleksabajat2001@gmail.com>
2023-01-20 07:50:00 +0100

    Account

--- Engine/models.py
+++ Engine/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model):        
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     lastName = db.Column(db.String, nullable=False)
@@ -14,4 +14,12 @@ class User(db.Model):
     phoneNumber = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     verified = db.Column(db.Boolean, default=False)
+    accounts = db.relationship('Account')
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    balance = db.Column(db.Integer, default=0)
+    currency = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
