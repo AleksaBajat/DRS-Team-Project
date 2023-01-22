5bef95e99d6235c7cc7335c275c0a30973d966da
PeskanovJ <jovan.peskanov@gmail.com>
2023-01-22 20:49:52 +0100

    login and registration validation

--- UI/static/js/login.js
+++ UI/static/js/login.js
@@ -1,8 +1,9 @@
 function login(){
     try{
         var form = document.getElementById('login-form');
-        var formData = new FormData(form);        
-    
+        var formData = new FormData(form);
+        if(!ValidateEmail())
+            throw Error('Invalid email address!');        
         $.ajax({
             type: "POST",        
             url: "/login",
@@ -11,9 +12,23 @@ function login(){
             contentType: false,
             success: function (response) {
                 window.location.href="/index" 
-            }
+            },
+            error: function (ajaxContext) {
+                alert(ajaxContext.responseText)
+            }      
         });
     }catch(e){
         console.log(e)
+    }
+}
+
+function ValidateEmail() {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    input = document.getElementById('email')
+    if (input.value.match(validRegex)) {
+        return true;
+    } else {
+        alert("Invalid email address!");
+        return false;
     }
 }
